e7678197f0a55c4261894c9228d60ae9807a0f38
Tatyana-Shevchuk <130395557+Tatyana-Shevchuk@users.noreply.github.com>
2023-11-24 20:57:26 +0300

    Delete module11_homework directory

--- module11_homework/Task_4.js
+++
@@ -1,5 +0,0 @@
-const intervalID = setInterval (function (a,b) {
-    for (let i = a; i <=b; i++) {
-      console.log (i)
-      clearInterval (intervalID);
-}}, 1000, 15, 78);
--- module11_homework/Task_5.js
+++
@@ -1,7 +0,0 @@
-const a = +prompt ('Введите число');
-const b = +prompt ('Введите степень числа');
-const sqrt = (a, b) => {
-  const result = Math.pow (a,b);
-  console.log (result);
-}
-sqrt(a, b)
